617585a6f1bba4189ff22845f27893532390f6a3
Guilherme de Oliveira Mendes <56366651+GG555-13@users.noreply.github.com>
2024-04-15 01:42:22 -0300

    Adicionando as partes para comparar
    adicionando as partes que permitem que o código compara os n dígitos adicionados na string com os já vistos e adiciona os novos, também retorna quando a strong quando todo o grafo for comparado

--- src/Safe/UnlockSafe.py
+++ src/Safe/UnlockSafe.py
@@ -1,6 +1,9 @@
 class Solution:
     def crackSafe(self, n: int, k: int) -> str:
         def dfs (res,vis,total):
+            if res[-n:] in vis:return
+            vis.add(res[-n:])
+            if len(vis) == total:return res            
             for i in range(k):
                 prox = dfs (res + str(i),vis,total)
                 if prox: return prox
