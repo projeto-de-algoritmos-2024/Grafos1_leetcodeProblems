ec1dad61328890b6de9515e5bb15c7f8a6fce1b9
Rodrigo Wright <wright.rodrigo380@gmail.com>
2024-04-14 23:42:06 -0300

    conta desconectados

--- src/Network/network.cpp
+++ src/Network/network.cpp
@@ -8,19 +8,30 @@ typedef struct node {
 } node;
 
 int main(){
-    int n = 4;
+    int n = 4;    
     vector<vector<int>> connections = {{0, 1}, {0, 2}, {1, 2}};
     vector<node*> network (n, NULL);
-    if (network[3] == NULL) cout << "Taquei o piru na portuquesa" << endl;
-    for (int i = 0; i < connections.size(); i++){
+    int unconected = 0;
 
+    for (int i = 0; i < connections.size(); i++){
         node *new1 = (node*)malloc(sizeof(node));
         new1->point = connections[i][1];
         new1->next = network[connections[i][0]];
         network[connections[i][0]] = new1;
+
         node *new2 = (node*)malloc(sizeof(node));
         new2->point = connections[i][0];
-        new2->next = network[connections[i][1]];   
+        new2->next = network[connections[i][1]];
+        network[connections[i][1]] = new2;   
     }
+
+    for (int i = 0; i < network.size(); i++){
+        if (network[i] == NULL) unconected++;
+    }
+    // cout << endl << network[1]->point << endl;
+    // cout << unconected << endl;
+    int teste = connections.size();
+    if (n - teste > 0) cout << unconected << endl;
+    else cout << -1;
     return 0;
 }
